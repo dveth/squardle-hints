d17559152a0c870682189f0a7258f66707cae2fd
dveth <kanosauries@gmail.com>
2024-11-25 20:51:58 -0600

    finished FilterWords

--- internals/squardle/squardle.go
+++ internals/squardle/squardle.go
@@ -1,9 +1,13 @@
 package squardle
 
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
 // Function that takes in a word and return whether it matches the specified conditions
-type WordFilter interface {
-	func(word string, substring string) bool
-}
+type WordFilter func(word string, substring string) bool
 
 func WordBegins(word string, substring string) bool {
 	if len(substring) > len(word) {
@@ -11,4 +15,34 @@ func WordBegins(word string, substring string) bool {
 	}
 
 	return word[:len(substring)] == substring
+}
+
+func WordEnds(word string, substring string) bool {
+	if len(substring) > len(word) {
+		return false
+	}
+	return word[len(word)-len(substring):] == substring
+}
+
+func GetFirstWordFromLine(line string) string {
+	line = strings.TrimSpace(line)
+	return strings.Split(line, " ")[0]
+}
+
+func FilterWords(filename string, filter WordFilter, substring string) ([]string, error) {
+	file, err := os.Open("NWL2020.txt")
+	if err != nil {
+		return []string{}, err
+	}
+	defer file.Close()
+
+	words := []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		firstWord := strings.ToLower(GetFirstWordFromLine(scanner.Text()))
+		if filter(firstWord, substring) {
+			words = append(words, firstWord)
+		}
+	}
+	return words, nil
 }
--- tests/squardle_test.go
+++ tests/squardle_test.go
@@ -1,7 +1,9 @@
 package squardle
 
 import (
+	"slices"
 	"squardle-hints/internals/squardle"
+	"strings"
 	"testing"
 )
 
@@ -38,5 +40,98 @@ func TestWordBegins(t *testing.T) {
 	got = squardle.WordBegins(word, substring)
 	if want != got {
 		t.Fatalf("For Word Begins, wanted %v, got %v for word %s and substring %s", want, got, word, substring)
+	}
+}
+
+func TestWordEnds(t *testing.T) {
+	word := "ending"
+	substring := "ing"
+
+	want := true
+	got := squardle.WordEnds(word, substring)
+
+	if want != got {
+		t.Fatalf("For Word Ends, wanted %v, got %v for word %s and substring %s", want, got, word, substring)
+	}
+
+	want = false
+	substring = "test"
+	got = squardle.WordEnds(word, substring)
+
+	if want != got {
+		t.Fatalf("For Word Ends, wanted %v, got %v for word %s and substring %s", want, got, word, substring)
+	}
+
+	want = true
+	substring = "ending"
+	got = squardle.WordEnds(word, substring)
+
+	if want != got {
+		t.Fatalf("For Word Ends, wanted %v, got %v for word %s and substring %s", want, got, word, substring)
+	}
+
+	want = false
+	substring = "dynamic test"
+	got = squardle.WordEnds(word, substring)
+
+	if want != got {
+		t.Fatalf("For Word Ends, wanted %v, got %v for word %s and substring %s", want, got, word, substring)
+	}
+}
+
+func TestGetFirstWordFromLine(t *testing.T) {
+	line := "dynamic music for 1 2 3"
+	want := "dynamic"
+
+	got := squardle.GetFirstWordFromLine(line)
+
+	if want != got {
+		t.Fatalf("For GetFirstWordFromLine, want %s, got %s for line %s", want, got, line)
+	}
+
+	line = " dynamic music for 1 2 3"
+
+	got = squardle.GetFirstWordFromLine(line)
+
+	if want != got {
+		t.Fatalf("For GetFirstWordFromLine, want %s, got %s for line %s", want, got, line)
+	}
+}
+
+func TestFilterWords(t *testing.T) {
+	want := []string{"beast", "beasts", "beastie", "beastly", "beasties", "beastings", "beastlier", "beastliest", "beastliness", "beastlinesses"}
+	substring := "beast"
+	filepath := "./NWL2020.txt"
+	got, err := squardle.FilterWords(filepath, squardle.WordBegins, substring)
+
+	if err != nil {
+		t.Fatalf("In FilterWords, got error: %s", err.Error())
+	}
+	if !slices.Equal(want, got) {
+		t.Logf("Lengths: want %d, got %d\n", len(want), len(got))
+		t.Fatalf("In Filterwords, want %v, got %v, for WordBegins and substring %s", want, got, substring)
+	}
+
+	substring = "dancing"
+	want = []string{"dancing", "outdancing", "ropedancing", "slamdancing", "breakdancing"}
+	got, err = squardle.FilterWords(filepath, squardle.WordEnds, substring)
+	if err != nil {
+		t.Fatalf("In FilterWords, got error: %s", err.Error())
+	}
+	if !slices.Equal(want, got) {
+		t.Logf("Lengths: want %d, got %d\n", len(want), len(got))
+		t.Fatalf("In Filterwords, want %v, got %v, for WordEnds and substring %s", want, got, substring)
+	}
+
+	substring = "verish"
+	want = []string{"feverish", "liverish", "cleverish", "feverishly", "impoverish", "feverishness", "impoverished", "impoverisher", "impoverishes", "liverishness",
+		"impoverishers", "impoverishing", "feverishnesses", "impoverishment", "liverishnesses", "impoverishments"}
+	got, err = squardle.FilterWords(filepath, strings.Contains, substring)
+	if err != nil {
+		t.Fatalf("In FilterWords, got error: %s", err.Error())
+	}
+	if !slices.Equal(want, got) {
+		t.Logf("Lengths: want %d, got %d\n", len(want), len(got))
+		t.Fatalf("In Filterwords, want %v, got %v, for WordContains and substring %s", want, got, substring)
 	}
 }
